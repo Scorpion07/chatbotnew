f39f52218eeaab469f6a946227198954b2a36afb
Sachin <sach.scorpion33@gmail.com>
2025-11-02 23:49:46 +0530

    fix(auth): use bcryptjs and remove node-fetch import to fix 404 from backend start

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,6 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import fetch from "node-fetch";
 import { User } from "../models/index.js";
 
 const router = express.Router();
