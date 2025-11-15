7fbe905edf09022ad0b2488f6e72923f6d09e6d9
Sachin <sach.scorpion33@gmail.com>
2025-11-16 01:33:28 +0530

    fix

--- frontend/src/lib/apiClient.js
+++ frontend/src/lib/apiClient.js
@@ -3,7 +3,7 @@ import config, { getApiUrl } from '../config';
 
 // Create axios instance using clean base URL
 const apiClient = axios.create({
-  baseURL: config.api.baseUrl + "/api",
+  baseURL: config.api.baseUrl,
   withCredentials: true,
 });
 
