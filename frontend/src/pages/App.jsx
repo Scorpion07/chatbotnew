eb785f8373c3aa9514c17481d4fbca420fb8ab20
Sachin <sach.scorpion33@gmail.com>
2025-11-16 02:58:10 +0530

    fix

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import { config, getApiUrl } from '../config.js';
+import config, { getApiUrl } from '../config.js';
 import axios from 'axios';
 import Home from './Home.jsx';
 import Admin from './Admin.jsx';
